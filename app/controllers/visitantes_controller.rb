d5a97bc47dd93f9ec5321ce627f4c914e5a75089
EIIUCAB 06 <eiiucab06@Mac-mini-de-EIIUCAB-5.local>
2014-04-22 09:47:27 -0430

    Actualizar la pagina principla usando plantillas layouts y mensajes de alerts

--- app/controllers/visitantes_controller.rb
+++ app/controllers/visitantes_controller.rb
@@ -1,8 +1,12 @@
 class VisitantesController < ApplicationController 
 
 	def new 
+		
 		@propietario = Propietario.new
+		flash[:notice] = 'Bienvenido'
+		flash[:alert] = 'Mi cumple esta cerca'
 		render 'visitantes/new'
+
 	end
 
 end
